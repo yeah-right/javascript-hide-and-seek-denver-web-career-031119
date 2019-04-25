843eaec928fdad7bf9354b4a2f85612a35ff2862
yeah-right <claar.zack@gmail.com>
2019-04-25 13:24:20 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 
+function getFirstSelector(s) {
+    return document.querySelector(s);
+  }
+  
+  function nestedTarget() {
+    return document.querySelector('#nested .target');
+  }
+  
+  function deepestChild() {
+    let node = document.getElementById('grand-node');
+    let nextNode = node.children[0];
+  
+    while (nextNode) {
+      node = nextNode;
+      nextNode = node.children[0];
+    }
+  
+    return node;
+  }
+  
+  function increaseRankBy(n) {
+    const theRankedLists = document.querySelectorAll('.ranked-list');
+  
+    for (let i = 0, l = theRankedLists.length; i < l; i++) {
+      let children = theRankedLists[i].children;
+  
+      for (let j = 0, k = children.length; j < k; j++) {
+        children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+      }
+    }
+  }
